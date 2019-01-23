96a37ae7e2b662096787271057d7693fb25fcccc
LiuWenzhuo <43992811+LiuWenzhuo@users.noreply.github.com>
2019-01-23 21:30:10 +0100

    Add files via upload

--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -1,57 +1,25 @@
-class FeatureExtractor(object):
+from __future__ import division
+
+from sklearn.base import BaseEstimator
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+import lightgbm as lgb
+
+
+class Classifier(BaseEstimator):
     def __init__(self):
-        pass
+        
+        self.model = lgb.LGBMClassifier(
+        boosting_type='gbdt', num_leaves=31, reg_alpha=0.0, reg_lambda=1,
+        max_depth=-1, n_estimators=1500, objective='binary',
+        subsample=0.7, colsample_bytree=0.7, subsample_freq=1,
+        learning_rate=0.05, min_child_weight=50, random_state=2018, n_jobs=-1
+    )
 
-    def fit(self, X_df, y):
-        return self
+    def fit(self, X, y):
+        self.model.fit(X, y)
 
-    def transform(self, X_df):
-        X_df_new = X_df[0].copy()
-        data_new=X_df[1].copy()
-
-        X_df_new=X_df_new.fillna('-1')#replace missing values NaN
-        data_new=data_new.fillna('-1')
-
-        one_hot_feature=['LBS','age','carrier','consumptionAbility','education','gender','house','os','ct','marriageStatus','advertiserId','campaignId', 'creativeId',
-       'adCategoryId', 'productId', 'productType']#features with only one scalar
-        vector_feature=['appIdAction','appIdInstall','interest1','interest2','interest3','interest4','interest5','kw1','kw2','kw3','topic1','topic2','topic3']#vector features
-
-        X_df_new=labelEncoder(data_new,X_df_new,one_hot_feature)
-        data_new=labelEncoder(data_new,data_new,one_hot_feature)#normalize features
-
-        X_sparse=OneHot(data_new,X_df_new,one_hot_feature)
-        X_sparse=Vectorize(data_new,X_df_new,vector_feature,X_sparse)
-
-        return X_sparse
-
-def labelEncoder(data,X_df,one_hot_feature):#normalize features
-    le=LabelEncoder()
-    for feature in one_hot_feature:
-        try:
-            le.fit(data[feature].apply(int))
-            #data[feature]=le.transform(data[feature].apply(int))
-            X_df[feature]=le.transform(X_df[feature].apply(int))
-        except:
-            le.fit(data[feature])
-            #data[feature]=le.transform(data[feature])
-            X_df[feature]=le.transform(X_df[feature])
-    return X_df
-
-def OneHot(data,X_df,one_hot_feature):
-    enc=OneHotEncoder()
-    X_sparse=X_df[['creativeSize']]
-    for feature in one_hot_feature:
-        enc.fit(data[feature].values.reshape(-1, 1))
-        X_onehot=enc.transform(X_df[feature].values.reshape(-1, 1))
-        X_sparse=sparse.hstack((X_sparse, X_onehot))
-    print('one hot finished')
-    return X_sparse
-
-def Vectorize(data,X_df,vector_feature,X_sparse):
-    cv=CountVectorizer()
-    for feature in vector_feature:
-        cv.fit(data[feature])
-        X_vec=cv.transform(X_df[feature])
-        X_sparse=sparse.hstack((X_sparse,X_vec))
-    print('cv finished')
-    return X_sparse
+    def predict_proba(self, X):
+        return self.model.predict_proba(X)
+        
